b38150c0bf59dc7d8aaafb10196e997747a0e11d
unknown <gaiedhazem@gmail.com>
2022-04-09 23:47:39 +0000

    back-end payement

---
+++ server/src/main/java/isi/com/Qatar2022/payment/PaymentGatewayController.java
@@ -0,0 +1,24 @@
+package isi.com.Qatar2022.payment;
+
+import com.stripe.model.Charge;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@CrossOrigin("*")
+@RequestMapping("/api/payment")
+public class PaymentGatewayController {
+    
+    private StripeClient stripeClient;
+    @Autowired
+    PaymentGatewayController(StripeClient stripeClient) {
+        this.stripeClient = stripeClient;
+    }
+    @PostMapping("/charge")
+    @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
+    public Charge chargeCard(@RequestHeader(value="token") String token, @RequestHeader(value="amount") Double amount) throws Exception {
+        return this.stripeClient.chargeNewCard(token, amount);
+    }
+}
